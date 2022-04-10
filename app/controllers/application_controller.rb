14774f8dc721eac3c8f8eee1ebd9f5bce8bd21ca
Alex Lutz <alexinslc@gmail.com>
2022-04-10 23:48:39 +0000

    setup current_user and basic formatting for navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token
+    before_action :set_current_user
+
+    def set_current_user
+        if session[:user_id]
+            Current.user = User.find(session[:user_id])
+        end
+    end
 end
