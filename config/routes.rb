ff474ee7eb2890b2dc1c8c7f152c8ed2920e3981
Alex Lutz <alexinslc@gmail.com>
2022-04-20 21:15:55 -0600

    adds edit password

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
   
