e1e0ae0a04b940bc7f5307134f9cb56d986ed72a
Alex Lutz <alexinslc@gmail.com>
2022-04-21 23:26:06 -0600

    Setup Background Jobs using Sidekiq

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,11 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  # Update Time Zone for local development 
+  config.time_zone = "Mountain Time (US & Canada)"
+  # Setup default URL options for local development
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  # Setup ActiveJob to use the Sidekiq adapter
+  config.active_job.queue_adapter = :sidekiq
 
 end
